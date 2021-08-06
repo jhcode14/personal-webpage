1b7464412a8a8832317b62b009076fbccbfaf581
jhcode14 <jhcode14@github.com>
2021-08-06 10:17:03 -0700

    updates

--- pages/blog.js
+++ pages/blog.js
@@ -14,22 +14,23 @@ const blogs = () => {
             <div className={styles.grid}>
                 <h1>Blog</h1>
                 <div className={styles.picture}>
-                    <img src="/taipe.jpg">
+                    <img src="/taipei.jpg">
                     </img>
                 </div>
                 <div className={styles.bigSpace}>
                     <p>
-                       Hey there! Thanks for stopping by my little blog post! I'd like to share this photo I took back when I was traveling in Taiwan in a city called Taipei.
-                        Taipei is the capital of Taiwan, where the traffic and the streets are always busy.
-                        By taking this picture, I hope you can see the essence and the beauty
-                        of Taiwan.
+                        Hey there! Thanks for stopping by my little blog post! I'd like to share this photo
+                        I took back when I was traveling in Taiwan in a city called Taipei. Taipei is the
+                        capital of Taiwan, where the traffic and the streets are always busy. By taking this
+                        picture, I hope you can see the essence and the beauty of Taiwan.
                         <br />
                         <br />
                         I started to fall in love with photography during my freshman year of my college.
                         Sometimes when I walk out of the building after a class or a long period of studying,
-                        I looked up at the sky and realized how I took the beauty of California sunsets for
+                        I looked up at the beautiful sky and realized how I took the beauty of California for
                         granted. With the help of my roommate Eric, who introduced me to Digital Single-Lens
-                        Reflex Cameras (DSLR), I started to develop a passion for exploring the beauty of nature.
+                        Reflex Cameras (DSLR), I started to develop a passion for taking pictures and
+                        exploring the beauty of nature.
                     </p>
                 </div>
 
--- pages/projects.js
+++ pages/projects.js
@@ -24,17 +24,26 @@ const projects = () => {
           {" "}
           ---
           Chicken_Bake [In Progress]
-          This project is the continuation of the Costco_Item Tracker project I have made. The project aims to become the camelcamelcamel.com for Costco. Because similar to amazon.com, Costco also does not always show price changes of items they are selling. The project's goal is to let shoppers search Costco items on this webpage and see the current price and the price fluctuation history to determine if the current time is the best time to purchase the thing at Costco.
+          This project is the continuation of the Costco_Item Tracker project I have made.
+          The project aims to become the camelcamelcamel.com for Costco. Because similar to
+          amazon.com, Costco also does not always show price changes of items they are
+          selling. The project's goal is to let shoppers search Costco items on this webpage
+          and see the current price and the price fluctuation history to determine if the
+          current time is the best time to purchase the thing at Costco.
           <br />
           <br />
           https://github.com/jhcode14/personal-webpage
           Personal Webpage
-          This project is this website that you are on right now! This website is built with JavaScript, TypeScript, CSS, HTML, Next.JS, React.JS. Feel free to look at how I built it with the GitHub link above!
+          This project is this website that you are on right now! This website is built with
+          JavaScript, TypeScript, CSS, HTML, Next.JS, React.JS. Feel free to look at how I
+          built it with the GitHub link above!
           <br />
           <br />
           https://github.com/jhcode14/Costco_Item_Tracker
           Costco_Item_Tracker
-          This project is a local Python-based software that allows users to search through a Costco web page category and record the price. Shoppers can compare prices with their saved price histories to compare if Costco currently holds the best price for the item.
+          This project is a local Python-based software that allows users to search through a
+          Costco web page category and record the price. Shoppers can compare prices with their
+          saved price histories to compare if Costco currently holds the best price for the item.
 
         </p>
       </div>
