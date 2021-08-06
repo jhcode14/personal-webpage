c23c50952daac1b6805c07c1a2714ab23676d5e3
jhcode14 <69288926+jhcode14@users.noreply.github.com>
2021-08-05 22:13:29 -0700

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -22,25 +22,22 @@ const about = () => {
 
         <p className={styles.space}>
           {" "}
-          Hi, I'm Jason, I am currently studying at UC San Diego pursuing B.S.
+          Hi, I'm Jason. I am currently studying at UC San Diego, pursuing B.S.
           in Machine Learning and a minor in Computer Science. My primary
           interest currently is to pursue a Software Development Internship, as
           I aspire to be a software engineer in the future.
           <br />
           <br />
-          Before transferring to UC San Diego, I studied at UC Riverside, where
-          I got to experience medical science, research, and biomedical
-          engineering. Through working at a emergency department, researching at
+          Before transferring to UC San Diego, I studied at UC Riverside. Through working at an emergency department, researching at
           UCR School of Medicine stroke lab, and volunteering at the local
-          community. I learned to manage time wisely, deal with unexpected problems
-          and be a team oriented person.
+          community. I learned to manage time wisely, deal with unexpected problems and be a team-oriented person.
           <br />
           <br />
           In my spare time, I enjoy playing guitar and online games with
-          friends. I also enjoy photography and running (I look forward to run a
+          friends. I also enjoy photography and running (I look forward to running a
           half-marathon in the near future!). I like to try new food and meet
-          new people so feel free to reach out! You can reach me through the
-          social-websites below!
+          new people, so feel free to reach out! You can reach me through the
+          social websites below!
         </p>
       </div>
     </div>
